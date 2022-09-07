24862cbe64921e7d407f3aedd03a3792fc86c42b
Jack <406151651@qq.com>
2022-09-07 16:00:48 +0800

    添加或者修改项目代码。

---
+++ src/main/java/com/mp/config/MyBatisPlusConfiguration.java
@@ -0,0 +1,16 @@
+package com.mp.config;
+
+//import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+//import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisPlusConfiguration {
+
+//    @Bean
+//    public ISqlInjector sqlInjector() {
+//        return new LogicSqlInjector();
+//    }
+
+}
--- src/main/java/com/mp/entity/User.java
+++ src/main/java/com/mp/entity/User.java
@@ -2,6 +2,7 @@ package com.mp.entity;
 
 import java.time.LocalDateTime;
 
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -10,23 +11,33 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = false)
 public class User extends Model<User> {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	//主键
-	private Long id;
-	
-	//姓名
-	private String name;
-	
-	//年龄
-	private Integer age;
-	
-	//邮箱
-	private String email;
-	
-	//直属上级
-	private Long managerId;
-	
-	//创建时间
-	private LocalDateTime createTime;
+    //主键
+    private Long id;
+
+    //姓名
+    private String name;
+
+    //年龄
+    private Integer age;
+
+    //邮箱
+    private String email;
+
+    //直属上级id
+    private Long managerId;
+
+    //创建时间
+    private LocalDateTime createTime;
+
+    //更新时间
+    private LocalDateTime updateTime;
+
+    //版本
+    private Integer version;
+
+    //逻辑删除标识（0->未删除，1->删除）
+    @TableLogic
+    private Integer deleted;
 }
